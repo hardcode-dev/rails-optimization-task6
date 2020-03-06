b96828d507744fded657113cab4408634eeecec4
Konstantin Bludov <konstantin.bludov@binomo.com>
2020-03-06 10:40:53 +0300

    Add analyzer

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,7 +1,16 @@
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const environment = require('./environment');
+
+environment.plugins.append(
+  'BundleAnalyzerPlugin',
+  new BundleAnalyzerPlugin({
+    analyzerMode: 'static',
+    reportFilename: '../../tmp/report.html',
+  }),
+);
+
 const config = environment.toWebpackConfig();
 
 // For more information, see https://webpack.js.org/configuration/devtool/#devtool
 config.devtool = 'eval-source-map';
-
 module.exports = config;
