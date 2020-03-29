062e4db6c38cf280728e515b6c5772f2db22d050
Dmitry Badichan <badimalex@gmail.com>
2020-03-29 21:39:38 +0200

    Fix dashboard/pro perfomance

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -4,4 +4,7 @@ const config = environment.toWebpackConfig();
 // For more information, see https://webpack.js.org/configuration/devtool/#devtool
 config.devtool = 'eval-source-map';
 
+// node_modules/.bin/webpack --config config/webpack/production.js --profile --json > stats.json
+// node_modules/.bin/webpack-bundle-analyzer stats.json
+
 module.exports = config;
