6e1334207957343ec00974dfe3d6ec97563146ed
m.oleynik <m.oleynik@fun-box.ru>
2020-08-19 21:58:36 +0300

    Подготовка к оптимизации moment.js

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,4 +1,16 @@
 const environment = require('./environment');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin;
+
+environment.plugins.append(
+  'BundleAnalyzer',
+  new BundleAnalyzerPlugin({
+    analyzerMode: 'static',
+    reportFilename: '/home/mikeoil/opt/rails-optimization-task6/report.html',
+    openAnalyzer: true,
+  }),
+);
+
 const config = environment.toWebpackConfig();
 
 // For more information, see https://webpack.js.org/configuration/devtool/#devtool
