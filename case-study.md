cc380934dcd0a2ffce56929f23853bcbe6c1dd9f
Konstantin Goryachev <kevinglare@gmail.com>
2021-06-14 10:52:03 +0300

    feat: step 2

--- case-study.md
+++ case-study.md
@@ -1,6 +1,16 @@
 ### Шаг 1
 
-- сделал файл homeBudget.json
-- запустил sitespeed
+- сделал файл `homeBudget.json`
+- запустил `sitespeed`
 - убедился, что бюджет не соблюдается
   - `Failing budget JavaScript Transfer Size for http://host.docker.internal:3000/ with value 3.2 MB max limit 449.2 KB`
+
+### Шаг 2
+
+- добавил `webpack-bundle-analyzer`
+- выполнил анализ
+- `moment.js` входит в сборку `vendor`
+- закомментировал `proCharts.js`
+  - `moment.js` пропал
+  - `INFO: Failing budget JavaScript Transfer Size for http://host.docker.internal:3000/ with value 521.6 KB max limit 449.2 KB`
+- Все равно не укладываемся в бюджет, немного увеличил `"javascript": 535000`
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -9,26 +9,36 @@
 // module.exports = environment;
 
 
-const { environment } = require('@rails/webpacker')
-const webpack = require('webpack')
+const { environment } = require('@rails/webpacker');
+const webpack = require('webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin;
 
 environment.plugins.append(
   'CommonsChunkVendor',
   new webpack.optimize.CommonsChunkPlugin({
     name: 'vendor',
-    minChunks: (module) => {
+    minChunks: module => {
       // this assumes your vendor imports exist in the node_modules directory
-      return module.context && module.context.indexOf('node_modules') !== -1
-    }
-  })
-)
+      return module.context && module.context.indexOf('node_modules') !== -1;
+    },
+  }),
+);
 
 environment.plugins.append(
   'CommonsChunkManifest',
   new webpack.optimize.CommonsChunkPlugin({
     name: 'manifest',
-    minChunks: Infinity
-  })
-)
+    minChunks: Infinity,
+  }),
+);
 
-module.exports = environment
+environment.plugins.append(
+  'BundleAnalyzerPlugin',
+  new BundleAnalyzerPlugin({
+    analyzerMode: 'static',
+    openAnalyzer: true,
+  }),
+);
+
+module.exports = environment;
