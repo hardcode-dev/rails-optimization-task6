e0f153e33593cb4732352f9b0a3820f78f9e2c29
Sergey <5695559@mail.ru>
2021-10-31 01:36:24 +0500

    hw6

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -4,4 +4,13 @@ const config = environment.toWebpackConfig();
 // For more information, see https://webpack.js.org/configuration/devtool/#devtool
 config.devtool = 'eval-source-map';
 
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+environment.plugins.append(
+  'BundleAnalyzer',
+  new BundleAnalyzerPlugin({
+    analyzerMode: 'static',
+    openAnalyzer: true,
+  }),
+);
+
 module.exports = config;
