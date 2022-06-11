84a04ade02725029139fa3f25a3a60e4e2526748
Alexander Belozerov <belozerov.sasha@gmail.com>
2022-06-11 21:03:21 +0600

    Optimization + CI

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -4,4 +4,12 @@ const config = environment.toWebpackConfig();
 // For more information, see https://webpack.js.org/configuration/devtool/#devtool
 config.devtool = 'eval-source-map';
 
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = {
+  plugins: [
+    new BundleAnalyzerPlugin({ analyzerMode: 'static', openAnalyzer: true })
+  ]
+}
+
 module.exports = config;
