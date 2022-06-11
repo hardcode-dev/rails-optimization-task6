99f05f4b7c8219f19c3a42c97a1c245d5f545870
Aleksandr Kariakin <aleksandr.kariakin@toptal.com>
2022-06-11 10:54:30 +0300

    Karyakin Aleksandr (sasha370)

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,7 +1,9 @@
+// eslint-disable-next-line no-undef
 const environment = require('./environment');
 const config = environment.toWebpackConfig();
 
 // For more information, see https://webpack.js.org/configuration/devtool/#devtool
 config.devtool = 'eval-source-map';
 
+// eslint-disable-next-line no-undef
 module.exports = config;
