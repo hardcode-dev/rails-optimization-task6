812b029bff9862830639d3aa422a6ad44113eddc
Efremov Evgenii <e.efremov@3commas.io>
2022-07-03 15:28:43 +0300

    revert standalone chart.js

--- app/javascript/packs/proCharts.js
+++ app/javascript/packs/proCharts.js
@@ -1,4 +1,4 @@
-import Chart from 'chart.js/dist/Chart.js';
+import Chart from 'chart.js';
 
 const reactionsCanvas = document.getElementById('reactionsChart');
 const commentsCanvas = document.getElementById('commentsChart');
