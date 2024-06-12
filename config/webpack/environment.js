2bdc211442a96d6bbfbe87eaa69d4002236e3375
potashin <potashin.nikita@gmail.com>
2024-06-12 16:27:47 +0300

    chore: profile & exclude chart.js/moment from vendor pack

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -18,7 +18,10 @@ environment.plugins.append(
     name: 'vendor',
     minChunks: (module) => {
       // this assumes your vendor imports exist in the node_modules directory
-      return module.context && module.context.indexOf('node_modules') !== -1
+      return module.context && 
+        module.context.indexOf('node_modules') !== -1 &&
+        module.context.indexOf('chart.js') === -1 &&
+        module.context.indexOf('moment') === -1
     }
   })
 )
@@ -30,5 +33,14 @@ environment.plugins.append(
     minChunks: Infinity
   })
 )
+
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+environment.plugins.append(
+  'BundLeAnalyzer',
+  new BundleAnalyzerPlugin({
+    analyzerMode: 'static',
+    openAnalyzer: true,
+  }),
+);
 
 module.exports = environment
